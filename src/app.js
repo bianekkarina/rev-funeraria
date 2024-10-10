c2aef920d2d185fc655238a12d122bdcf4e751a7
Karina Bianek <bianekkarina@gmail.com>
2024-10-10 11:43:07 -0300

    Atualizar o app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,31 @@
-import express from 'express'
+// a execução do programa começa aqui
+
+/*
+importações: recomendo que vá importando conforme for usando para evitar
+importações desnecessárias
+*/
+import express from 'express';
 import connectDB from './config/db.js'
-import vivosRouter from './routers/vivosRouter.js'
-import mortosRouter from './routers/mortosRouter.js'
-import enterroRouter from './routers/enterroRouter.js'
+import vivosRouter from './routers/vivosRouter.js';
+import mortosRouter from './routers/mortosRouter.js';
+import enterroRouter from './routers/enterroRouter.js';
 
-connectDB()
+// conectar com o banco de dados
+connectDB();
 
-const app = express()
+/*
+configurar o express - não se esquecer do express.json() para poder receber
+JSON no corpo da requisição
+*/
+const app = express();
+app.use(express.json());
 
-app.use(express.json())
-
-app.use('/vivos', vivosRouter)
+// roteadores responsaveis por redirecionar as requisições para os controladores
+app.use('/vivos', vivosRouter);
 app.use('/mortos', mortosRouter)
 app.use('/enterro', enterroRouter)
 
-app.listen(3000, () => console.log(`Servidor rodando na porta 3000`))
+// iniciar o servidor
+app.listen(3000, () => console.log('Server running on port 3000'));
+
+// não tem como colocar comentários no json, então ficou no campo description
