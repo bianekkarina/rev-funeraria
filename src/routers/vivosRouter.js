7c0316635bd681ded74ddc2b850473a17967ffaa
Karina Bianek <bianekkarina@gmail.com>
2024-10-10 12:05:27 -0300

    Atualizar o vivosRouter.js

--- src/routers/vivosRouter.js
+++ src/routers/vivosRouter.js
@@ -1,16 +1,18 @@
-import { Router } from 'express'
-import { 
+import { Router } from 'express';
+import {
     store,
     index,
     update,
     destroy
-} from '../controllers/vivosControllers.js'
+} from '../controllers/vivosController.js';
 
-const router = Router()
+const router = Router();
 
-router.post('/', store)
-router.get('/', index)
-router.put('/:id', update)
-router.delete('/:id', destroy)
+router.post('/', store);
+router.get('/', index);
+router.put('/:id', update);
+router.delete('/:id', destroy);
 
-export default router
+export default router;
+
+// os mesmos comentários do enterroRouter servem para cá
