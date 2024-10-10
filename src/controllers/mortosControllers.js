faced6d4d5cfa4826f70dc3c47c3d50bdc3d0dec
Karina Bianek <bianekkarina@gmail.com>
2024-10-10 11:46:39 -0300

    Atualizar o mortosControllers.js

--- src/controllers/mortosControllers.js
+++ src/controllers/mortosControllers.js
@@ -1,37 +1,39 @@
-import Morto from '../models/mortoModel.js'
+import Morto from '../models/mortoModel.js';
 
 export const store = async (req, res) => {
     try {
-        const morto = await Morto.create(req.body)
-        return res.status(201).json(morto)
+        const morto = await Morto.create(req.body);
+        return res.status(201).json(morto);
     } catch (error) {
-        return res.status(400).json({ message: error.message })
+        return res.status(400).json({ message: error.message });
     }
-}
+};
 
 export const index = async (req, res) => {
     try {
-        const morto = await Morto.find().exec()
-        return res.status(200).json(morto)
+        const mortos = await Morto.find().exec();
+        return res.status(200).json(mortos);
     } catch (error) {
-        return res.status(400).json({ message: error.message })
+        return res.status(400).json({ message: error.message });
     }
-}
+};
 
 export const update = async (req, res) => {
     try {
-        const morto = await Morto.findByIdAndUpdate(req.params.id, req.body).exec()
-        return res.status(200).json(morto)
+        const morto = await Morto.findByIdAndUpdate(req.params.id, req.body).exec();
+        return res.status(200).json(morto);
     } catch (error) {
-        return res.status(400).json({ message: error.message })
+        return res.status(400).json({ message: error.message });
     }
-}
+};
 
 export const destroy = async (req, res) => {
     try {
-        const morto = await Morto.findByIdAndDelete(req.params.id).exec()
-        return res.status(200).json(morto)
+        const morto = await Morto.findByIdAndDelete(req.params.id).exec();
+        return res.status(200).json(morto);
     } catch (error) {
-        return res.status(400).json({ message: error.message })
+        return res.status(400).json({ message: error.message });
     }
-}
+};
+
+// os comentários do vivosController valem para cá também
